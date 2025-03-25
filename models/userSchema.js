1e56c8c868061d0f867910479742284bcfbe1761
六六 <r@oni.run>
2025-03-25 14:52:12 +0100

    userschema

---
+++ models/userSchema.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    // tracks misc fun data for users globally
+    userId: { type: String, required: true, unique: true },
+    userName: { type: String, required: true },
+    gamesPlayed: { type: Number, default: 0 },
+    gamesWon: { type: Number, default: 0 },
+    longestGameName: { type: String, default: '' },
+    longestGameDuration: { type: Number, default: 0 },
+    currentWinStreak: { type: Number, default: 0 },
+});
+
+userSchema.index({ userId: 1 });
+
+module.exports = model('User', userSchema);
