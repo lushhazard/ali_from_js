dc9879f64e21bdd7effc4e4609b9a140f3cfb564
lushhazard <your@mom.gay>
2025-11-13 13:13:21 +0100

    path changes

--- commands/utility/endWatcher.js
+++ commands/utility/endWatcher.js
@@ -11,7 +11,19 @@ module.exports = {
             option.setName('url')
                 .setDescription('The website URL to stop watching')
                 .setRequired(true)
+                .setAutocomplete(true)
         ),
+
+    async autocomplete(interaction) {
+        const userId = interaction.user.id;
+        const watchers = await Watcher.find({ userId });
+        let choices = watchers.map(watcher => watcher.url);
+        const focusedOption = interaction.options.getFocused(true);
+        const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+        await interaction.respond(
+            filtered.map(choice => ({ name: choice, value: choice })),
+        );
+    },
     async execute(interaction) {
         const url = interaction.options.getString('url');
         const userId = interaction.user.id;
